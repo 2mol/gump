f6ab5530ae045cde4113d94e5790b14406a37c34
2mol <2mol@users.noreply.github.com>
2019-01-31 16:46:30 +0100

    bla

---
+++ src/CommonMath.hs
@@ -0,0 +1,44 @@
+
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module CommonMath where
+
+import qualified Data.Foldable as F
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+
+
+count :: forall t a . (Foldable t, Ord a)
+    => t a -> Map a Int
+count elements =
+    F.foldl' addCount M.empty elements
+    where
+        addCount :: Map a Int -> a -> Map a Int
+        addCount counter el = M.insertWith (+) el 1 counter
+
+
+proportions :: (Foldable t, Ord a) => t a -> [Double]
+proportions xs =
+    [c / n | c' <- counts, let c = fromIntegral c']
+    where
+        counts = M.elems (count xs)
+
+        n = fromIntegral $ length xs
+
+
+-- "impurity" measures:
+
+
+gini :: (Foldable t, Ord a) => t a -> Double
+gini xs =
+    F.sum [p * (1-p) | p <- proportions xs]
+
+
+entropy :: (Foldable t, Ord a) => t a -> Double
+entropy xs =
+    -F.sum [p * logBase 2 p | p <- proportions xs]
+
+
+misclassification :: (Foldable t, Ord a) => t a -> Double
+misclassification xs =
+    1 - maximum (proportions xs)
