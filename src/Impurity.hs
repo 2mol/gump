563c9ff604bec418270e3d7239f5dcc1605810f4
2mol <2mol@users.noreply.github.com>
2019-06-15 15:52:41 +0200

    rename math module

--- src/Impurity.hs
+++ src/Impurity.hs
@@ -2,45 +2,43 @@
 
 module Impurity where
 
-import qualified Data.Foldable as F
+import           Data.Foldable as F
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 
 
--- functions to help tally the number of occurrences
--- of list elements and their proportions:
-
-count :: forall t a . (Foldable t, Ord a)
-    => t a -> Map a Int
-count elements =
-    F.foldl' addCount M.empty elements
+-- count the number of occurrences of every list element
+tally :: forall t a . (Foldable t, Ord a) => t a -> Map a Int
+tally elements =
+    F.foldr' addCount M.empty elements
     where
-        addCount :: Map a Int -> a -> Map a Int
-        addCount counter el = M.insertWith (+) el 1 counter
+        addCount :: a -> Map a Int -> Map a Int
+        addCount el counter = M.insertWith (+) el 1 counter
 
 
+-- calculate which element occurs which percentage of the time
 proportions :: (Foldable t, Ord a) => t a -> [Double]
 proportions xs =
     [c / n | c' <- counts, let c = fromIntegral c']
     where
-        counts = M.elems (count xs)
-
+        counts = M.elems (tally xs)
         n = fromIntegral $ length xs
 
 
--- different impurity measures:
+-- entropy measure
+-- https://en.wikipedia.org/wiki/Entropy#Statistical_mechanics
+entropy :: Ord a => [a] -> Double
+entropy xs =
+    -sum [p * logBase 2 p | p <- proportions xs]
 
+
+-- gini coefficient as an impurity measure
 gini :: (Foldable t, Ord a) => t a -> Double
 gini xs =
     sum [p * (1 - p) | p <- proportions xs]
 
 
-entropy :: (Foldable t, Ord a) => t a -> Double
-entropy xs =
-    -sum [p * logBase 2 p | p <- proportions xs]
-
-
+-- misclassification coefficient as an impurity measure
 misclassification :: (Foldable t, Ord a) => t a -> Double
 misclassification xs =
     1 - maximum (proportions xs)
-
