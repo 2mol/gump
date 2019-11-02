d7962f910d741ac5ef60abdf358521159f9c50b1
Jasper Van der Jeugt <m@jaspervdj.be>
2019-11-02 17:53:08 +0100

    Graphviz

--- src/Feature.hs
+++ src/Feature.hs
@@ -1,13 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts #-}
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Csv as Csv
+import qualified Data.Graph.Inductive.Graph as Graph
 import qualified Data.HashMap.Strict as HMS
 import           Data.List
+import Control.Monad.RWS (runRWS)
+import Control.Monad.Writer (tell)
+import Control.Monad.State (state)
 import qualified Data.Map as Map
+import           Data.Ord (comparing)
 import qualified Data.Text as T
 import qualified Data.Vector as V
-import Data.Ord (comparing)
 import qualified Impurity
 
 --------------------------------------------------------------------------------
@@ -55,7 +60,7 @@ float = Feature
             else
                 let lo   = minimum floats
                     hi   = maximum floats
-                    step = (hi - lo) / 10.0 in
+                    step = (hi - lo) / 100.0 in
                 [ Split ("< " ++ show pivot) (">= " ++ show pivot) (\x -> x < pivot)
                 | pivot <- [lo + step, lo + 2 * step .. hi]
                 ]
@@ -125,6 +130,34 @@ data Tree a b t
     | Branch String (Split b) (Tree a b t) (Tree a b t)
     deriving (Show)
 
+treeToGraph
+    :: Show t => (t -> String) -> Tree a b t -> [String]
+treeToGraph mkLeafLabel tree =
+    let (_, _, strs) = runRWS (toGraph tree) () 0 in strs
+  where
+    toGraph tree = do
+        tell ["digraph graphname {"]
+        _ <- go tree
+        tell ["}"]
+
+    go (Leaf x d) = do
+        name <- fresh
+        let label = mkLeafLabel x ++ " (" ++ show d ++ ")"
+        tell [name ++ " [label=" ++ show label ++ "];"]
+        return name
+
+    go (Branch attr (Split ledge redge _) ltree rtree) = do
+        name <- fresh
+        tell [name ++ " [label=" ++ show attr ++ "];"]
+        lname <- go ltree
+        rname <- go rtree
+        tell [name ++ " -> " ++ lname ++ " [label=" ++ show ledge ++ "];"]
+        tell [name ++ " -> " ++ rname ++ " [label=" ++ show redge ++ "];"]
+        return name
+
+    fresh = state $ \counter -> ("node" ++ show counter, counter + 1)
+
+
 printTree :: Show t => Tree a b t -> [String]
 printTree (Leaf x d) = [show x ++ " (" ++ show d ++ ")"]
 printTree (Branch attr (Split ledge redge _) ltree rtree) =
@@ -176,8 +209,8 @@ main :: IO ()
 main = do
     content <- BL.readFile "data/iris.csv"
     irisses <- either fail return $ Csv.decode Csv.NoHeader content
-    putStrLn $ unlines $ printTree $ makeTree
-        6
+    writeFile "iris.dot" $ unlines $ treeToGraph T.unpack $ makeTree
+        3
         irisClass
         irisFeatures
         irisses
