15e0067c409c5bfb4495f96bc66ea730d9ff3c7a
Jasper Van der Jeugt <m@jaspervdj.be>
2019-11-02 17:05:32 +0100

    Irisses

--- src/Feature.hs
+++ src/Feature.hs
@@ -2,11 +2,13 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Csv as Csv
-import qualified Impurity
 import qualified Data.HashMap.Strict as HMS
 import           Data.List
+import qualified Data.Map as Map
 import qualified Data.Text as T
 import qualified Data.Vector as V
+import Data.Ord (comparing)
+import qualified Impurity
 
 --------------------------------------------------------------------------------
 
@@ -99,31 +101,83 @@ irisFeatures =
 
 gains
     :: forall a b t. Ord t => (a -> t) -> Feature a b -> V.Vector a
-    -> [(Split b, Double)]
+    -> [(Split b, (Double, V.Vector a, V.Vector a))]
 gains mkTarget feature datas = do
-    split@(Split _ _ f) <- splits feature (V.toList column)
+    split@(Split _ _ f) <- splits feature (map (extract feature) $ V.toList datas)
     return (split, gain split)
   where
-    column   = V.map (extract feature) datas
     target   = V.map mkTarget datas
     numTotal = fromIntegral (V.length target)
 
     targetEntropy = Impurity.entropy target
 
-    gain :: Split b -> Double
+    gain :: Split b -> (Double, V.Vector a, V.Vector a)
     gain (Split _ _ f) =
-        let (xs, ys)  = V.partition (f . fst) $ V.zip column target
+        let (xs, ys)  = V.partition (f . extract feature . fst) $ V.zip datas target
             propXs    = fromIntegral (V.length xs) / numTotal
             propYs    = fromIntegral (V.length ys) / numTotal
             xsEntropy = Impurity.entropy (V.map snd xs)
             ysEntropy = Impurity.entropy (V.map snd ys) in
-        targetEntropy - (propXs * xsEntropy + propYs * ysEntropy)
+        (targetEntropy - (propXs * xsEntropy + propYs * ysEntropy), V.map fst xs, V.map fst ys)
+
+data Tree a b t
+    = Leaf t Double
+    | Branch String (Split b) (Tree a b t) (Tree a b t)
+    deriving (Show)
+
+printTree :: Show t => Tree a b t -> [String]
+printTree (Leaf x d) = [show x ++ " (" ++ show d ++ ")"]
+printTree (Branch attr (Split ledge redge _) ltree rtree) =
+    [attr] ++
+    [" " ++ ledge] ++
+    map ("  " ++) (printTree ltree) ++
+    [" " ++ redge] ++
+    map ("  " ++) (printTree rtree)
+
+makeLeaf :: Ord t => (a -> t) -> V.Vector a -> Tree a b t
+makeLeaf mkTarget datas =
+    let tally           = Impurity.tally (fmap mkTarget datas)
+        total           = fromIntegral $ V.length datas
+        (target, count) = maximumBy (comparing snd) (Map.toList tally) in
+    Leaf target (fromIntegral count / total)
+
+makeTree
+    :: forall a b t. Ord t
+    => Int            -- ^ Max depth
+    -> (a -> t)       -- ^ Target
+    -> [Feature a b]  -- ^ Features
+    -> V.Vector a     -- ^ Data
+    -> Tree a b t
+makeTree maxDepth mkTarget features = go 0
+  where
+    go depth datas | depth >= maxDepth =
+        makeLeaf mkTarget datas
+
+    go depth datas | [single] <- nub (map mkTarget $ V.toList datas) =
+        Leaf single 1.0
+
+    go depth datas =
+        let allGains :: [(String, (Split b, (Double, V.Vector a, V.Vector a)))]
+            allGains =
+                [ (name feature, gain)
+                | feature <- features
+                , gain    <- gains mkTarget feature datas
+                ]
+
+            (fname, (split@(Split _ _ f), (_, xs, ys))) =
+                maximumBy (comparing (\(_, (_, (g, _, _))) -> g))
+                allGains in
+
+        Branch fname split
+            (go (depth + 1) xs)
+            (go (depth + 1) ys)
 
 main :: IO ()
 main = do
     content <- BL.readFile "data/iris.csv"
     irisses <- either fail return $ Csv.decode Csv.NoHeader content
-    putStrLn $ unlines $ map show $ gains
+    putStrLn $ unlines $ printTree $ makeTree
+        6
         irisClass
-        (float {extract = petalWidth, name = "petalWidth"})
+        irisFeatures
         irisses
